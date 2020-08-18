939d581431925b90e8b18bf7ab079060157ce83d
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-18 07:27:45 +0300

    add validate data to user model. add json files

--- models/User.js
+++ models/User.js
@@ -1,5 +1,26 @@
-let User = function(){
-  
+let User = function(data){
+  this.data = data;
+  this.errors = [];
+}
+
+User.prototype.validate = function(){
+  if(this.data.username == ""){
+    this.errors.push("Enter a username");
+  }
+  if(this.data.email == ""){
+    this.errors.push("Enter email");
+  }
+  if(this.data.password == ""){
+    this.errors.push("Enter the password");
+  }
+}
+
+User.prototype.register = function(){
+  // Validate Data
+  this.validate();
+  // Only if there are no validation errors
+  // then save the user data into a database
+
 }
 
 module.exports = User;
