dfda233efb8a17502bca0e91b8801fe788f3bc20
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-18 08:10:31 +0300

    download validator and add some validate methods

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const validator = require("validator");
 let User = function(data){
   this.data = data;
   this.errors = [];
@@ -7,11 +8,23 @@ User.prototype.validate = function(){
   if(this.data.username == ""){
     this.errors.push("Enter a username");
   }
+  if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)){
+    this.errors.push("Username can only contain numbers and letters!");
+  }
   if(this.data.email == ""){
     this.errors.push("Enter email");
   }
+  if(this.data.email != "" && !validator.isEmail(this.data.email)){
+    this.errors.push("Not Valide Email!")
+  }
+  if(this.data.password.length > 0 && this.data.password < 6){
+    this.errors.push("Password must be at least 6 characters");
+  }
   if(this.data.password == ""){
     this.errors.push("Enter the password");
+  }
+  if(this.data.username.length > 0 && this.data.username < 5){
+    this.errors.push("username must be at least 5 characters");
   }
 }
 
