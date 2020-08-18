6a3484eb172cb4f1d3c223dec1916581ec5fb0a9
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-18 08:40:01 +0300

    rid of any bogus properties

--- models/User.js
+++ models/User.js
@@ -4,32 +4,34 @@ let User = function(data){
   this.errors = [];
 }
 
-User.prototype.validate = function(){
-  if(this.data.username == ""){
-    this.errors.push("Enter a username");
-  }
-  if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)){
-    this.errors.push("Username can only contain numbers and letters!");
-  }
-  if(this.data.email == ""){
-    this.errors.push("Enter email");
-  }
-  if(this.data.email != "" && !validator.isEmail(this.data.email)){
-    this.errors.push("Not Valide Email!")
-  }
-  if(this.data.password.length > 0 && this.data.password < 6){
-    this.errors.push("Password must be at least 6 characters");
-  }
-  if(this.data.password == ""){
-    this.errors.push("Enter the password");
-  }
-  if(this.data.username.length > 0 && this.data.username < 5){
-    this.errors.push("username must be at least 5 characters");
+User.prototype.cleanUp = function(){
+  if(typeof(this.data.username) != "string"){this.data.username = "";}
+  if(typeof(this.data.email) != "string"){this.data.email = "";}
+  if(typeof(this.data.password) != "string"){this.data.password = "";}
+
+  // get rid of any bogus properties
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password
   }
 }
 
+User.prototype.validate = function(){
+  if(this.data.username == ""){this.errors.push("Enter a username");}
+  if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)){this.errors.push("Username can only contain numbers and letters!");}
+  if(this.data.email == ""){this.errors.push("Enter email");}
+  if(this.data.email != "" && !validator.isEmail(this.data.email)){this.errors.push("Not Valide Email!");}
+  if(this.data.password.length > 0 && this.data.password < 6){this.errors.push("Password must be at least 6 characters");}
+  if(this.data.password == ""){this.errors.push("Enter the password");}
+  if(this.data.username.length > 0 && this.data.username < 5){this.errors.push("username must be at least 5 characters");}
+}
+
+
+
 User.prototype.register = function(){
   // Validate Data
+  this.cleanUp();
   this.validate();
   // Only if there are no validation errors
   // then save the user data into a database
