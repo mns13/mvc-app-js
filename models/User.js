fbce064837314e8ab5a1371dec3d36e46c668ff8
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-19 17:44:58 +0300

    create a profile page [views]. create the find by username method[models/User].

--- models/User.js
+++ models/User.js
@@ -93,4 +93,29 @@ User.prototype.getAvatar = function(){
 }
 
 
+User.findByUsername = function(username){
+  return new Promise(function(resolve, reject){
+    if(typeof(username) != "string"){
+      reject();
+      return;
+    }
+    usersCollection.findOne({username: username}).then(function(userDoc){
+      if(userDoc){
+        userDoc = new User(userDoc, true);
+        userDoc = {
+          _id: userDoc.data._id,
+          username: userDoc.data.username,
+          avatar: userDoc.avatar
+        }
+        resolve(userDoc);
+      } else {
+        reject();
+      }
+    }).catch(function(){
+      reject();
+    });
+  });
+}
+
+
 module.exports = User;
