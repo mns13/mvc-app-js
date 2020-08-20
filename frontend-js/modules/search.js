cf4529dc9d363fb411b77d9f6fb5483eaf95142a
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-20 11:59:18 +0300

    work on Search class

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,5 +1,22 @@
 export default class Search {
+
+  // 1. Select DOM elements, and keep track of any useful data
   constructor(){
-    alert("Search js is successfully being executed");
+    this.headerSearchIcon = document.querySelector(".header-search-icon");
+    this.events();
+  }
+
+  // 2. Events
+  events(){
+    this.headerSearchIcon.addEventListener("click", (e )=> {
+      e.preventDefault();
+      this.openOverlay();
+    })
+  }
+
+
+  // 3. Methods
+  openOverlay(){
+    alert("openOverlay just ran!");
   }
 }
