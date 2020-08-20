f145837846cde724413058ef35b24bf7384236cd
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-20 16:38:02 +0300

    letting users follow each other and save a follow action into database

---
+++ models/Follow.js
@@ -0,0 +1,44 @@
+const usersCollection = require('../db').db().collection("users");
+const followsCollection = require('../db').db().collection("follows");
+const ObjectID = require('mongodb').ObjectID;
+
+
+
+let Follow = function(followedUsername, authorId){
+  this.followedUsername = followedUsername;
+  this.authorId = authorId;
+  this.errors = [];
+}
+
+Follow.prototype.create = function(){
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp();
+    await this.validate();
+    if(!this.errors.length){
+     await  followsCollection.insertOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)});
+     resolve();
+    } else {
+      reject(this.errors);
+    }
+  })
+}
+
+Follow.prototype.cleanUp = function(){
+  if(typeof(this.followedUsername) != "string"){
+    this.followedUsername = "";
+  }
+}
+
+Follow.prototype.validate = async function(){
+  // followed username must exists in database
+  let followedAccount = await usersCollection.findOne({username: this.followedUsername});
+  if(followedAccount){
+    this.followedId = followedAccount._id;
+  } else {
+    this.errors.push("You can not follow a user that does not exists");
+  }
+}
+
+
+
+module.exports = Follow;
