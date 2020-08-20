609ed0fb8d1e43d884173fd3c8593e54c62f3a82
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-20 20:47:07 +0300

    now we've loged chat message. next step store those in chat

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -4,19 +4,35 @@ export default class Chat {
     this.chatWrapper = document.querySelector('#chat-wrapper');
     this.openIcon = document.querySelector(".header-chat-icon");
     this.injectHTML();
+    this.chatField = document.querySelector("#chatField");
+    this.chatForm = document.querySelector("#chatForm");
     this.closeIcon = document.querySelector(".chat-title-bar-close");
     this.events();
   }
 
   // Events
   events(){
+    this.chatForm.addEventListener("submit", (e)=>{
+      e.preventDefault();
+      this.sendMessageToServer();
+    });
     this.openIcon.addEventListener("click", ()=>this.showChat());
     this.closeIcon.addEventListener("click", ()=>this.hideChat());
   }
 
   // Methods
+
+  sendMessageToServer(){
+    this.socket.emit('chatMessageFromBrowser', {message: this.chatField.value});
+    this.chatField.value = '';
+    this.chatField.focus();
+  }
+
   openConnection(){
     this.socket = io();
+    this.socket.on('chatMessageFromServer', function(data){
+      alert(data.message);
+    });
   }
 
   showChat(){
